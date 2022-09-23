34c08bf0cf4730c68b9a70c49fc159e88df134da
crltsnch <crltsnch@myuax.com>
2022-09-23 16:03:25 +0200

    ejercicio 1

---
+++ Ejercicio tema 1.py
@@ -0,0 +1,23 @@
+#matriz = [
+# [1, 1, 1, 3],
+# [2, 2, 2, 7],
+# [3, 3, 3, 9],
+# [4, 4, 4, 13]
+# ]
+
+#Ejercicio 1: La siguiente matriz (o lista con listas anidadas) debe cumplir una condición, 
+# y es que en cada fila el cuarto elemento siempre debe ser el resultado de sumar los tres primeros.
+# ¿Eres capaz de modificar las sumas incorrectas utilizando la técnica del slicing?
+
+matriz = [
+ [1, 1, 1, 3],
+ [2, 2, 2, 7],
+ [3, 3, 3, 9],
+ [4, 4, 4, 13]
+]
+
+matriz[1][-1] = sum(matriz[1][:-1])
+matriz[3][-1] = sum(matriz[3][:-1])
+print(matriz)
+
+#Ejercicio 2: Utilizando operadores lógicos
