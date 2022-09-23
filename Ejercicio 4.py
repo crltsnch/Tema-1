a110be5e074eb0878015570d66cbc67436601d5c
crltsnch <crltsnch@myuax.com>
2022-09-23 21:42:33 +0200

    ejercicio 4

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -11,3 +11,15 @@
 import sys
 from pyparsing import col
 
+if len(sys.argv) == 3:
+    filas = int(sys.argv[1])
+    columnas = int(sys.argv[2])
+    if filas<1 or filas>9 or columnas>9:
+        print("Error")
+    else:
+        for i in range(filas):
+            print(" ")
+            for c in range(columnas):
+                print(" * ", end='')
+else:
+    print("Error")
